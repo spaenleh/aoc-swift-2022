b0e515353005006500af76095ef6854c53e109a9
spaenleh <spaenleh@gmail.com>
2024-11-16 10:20:28 +0100

    bootstrap day7

---
+++ Tests/Day07.swift
@@ -0,0 +1,44 @@
+import Testing
+
+@testable import aoc
+
+@Suite("Day07")
+struct Day07Tests {
+  let testData = """
+    $ cd /
+    $ ls
+    dir a
+    14848514 b.txt
+    8504156 c.dat
+    dir d
+    $ cd a
+    $ ls
+    dir e
+    29116 f
+    2557 g
+    62596 h.lst
+    $ cd e
+    $ ls
+    584 i
+    $ cd ..
+    $ cd ..
+    $ cd d
+    $ ls
+    4060174 j
+    8033020 d.log
+    5626152 d.ext
+    7214296 k
+    """
+
+  @Test("part1")
+  func testPart1() async throws {
+    let challenge = Day07(data: testData)
+    #expect(challenge.part1() == 95437)
+  }
+
+  @Test("part2")
+  func testPart2() async throws {
+    let challenge = Day07(data: testData)
+    #expect(challenge.part2() == 0)
+  }
+}
